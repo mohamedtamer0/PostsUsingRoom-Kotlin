dd6af9134b9df74f64c77223598d1ae16a56f14d
mohamed tamer0 <mt4452242@gmail.com>
2021-03-20 00:23:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/postsusingroom_kotlin/Post.kt
@@ -0,0 +1,12 @@
+package com.example.postsusingroom_kotlin
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "posts_table")
+class Post(var userId: Int, var title: String, var body: String) {
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
+
+}
---
+++ app/src/main/java/com/example/postsusingroom_kotlin/PostsDao.kt
@@ -0,0 +1,17 @@
+package com.example.postsusingroom_kotlin
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import io.reactivex.Completable
+import io.reactivex.Single
+
+
+@Dao
+interface PostsDao {
+    @Insert
+    fun insertPost(post: Post?): Completable?
+
+    @Query("select * from posts_table")
+    fun getPosts(): Single<List<Post>>
+}
